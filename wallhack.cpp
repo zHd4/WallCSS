dedaca68336f2a28f4c4660dbb10bc6c6c1c5327
zHd4 <computersystembase@gmail.com>
2024-12-18 15:08:55 +0100

    Split definition and implementation of wallhack

---
+++ wallhack.cpp
@@ -0,0 +1,76 @@
+#include "wallhack.h"
+
+MemAccessException::MemAccessException(const string& msg) : message(msg) {}
+
+const char* MemAccessException::what() const noexcept override {
+    return message.c_str();
+}
+
+void Wallhack::setStateFromMemory() {
+    ReadProcessMemory(hProcess, (void*)wallhackAddr, &state, sizeof(state), nullptr);
+}
+
+void Wallhack::changeDrawMode(short mode, short* statePtr) {
+    MEMORY_BASIC_INFORMATION mbi;
+
+    if (VirtualQueryEx(hProcess, (LPVOID)wallhackAddr, &mbi, sizeof(mbi)) == 0) {
+        throw MemAccessException("Error: invalid address " + to_string(wallhackAddr) + ". Code: " + to_string(GetLastError()));
+    }
+
+    if (mbi.State != MEM_COMMIT) {
+        throw MemAccessException("Error: memory is not committed");
+    }
+
+    DWORD oldProtect;
+
+    if (VirtualProtectEx(hProcess, (LPVOID)wallhackAddr, sizeof(mode), PAGE_READWRITE, &oldProtect)) {
+        if (!WriteProcessMemory(hProcess, (LPVOID)wallhackAddr, &mode, sizeof(mode), nullptr)) {
+            throw MemAccessException("Error: cannot write value in memory. Code: " + to_string(GetLastError()));
+        }
+
+        *statePtr = mode;
+    }
+    else {
+        throw MemAccessException("Error: Cannot unprotect memory. Code: " + to_string(GetLastError()));
+    }
+
+    VirtualProtectEx(hProcess, (LPVOID)wallhackAddr, sizeof(mode), oldProtect, &oldProtect);
+}
+
+Wallhack::Wallhack(const Config& config) {
+    hWindow = FindWindowA(nullptr, config.window.c_str());
+    GetWindowThreadProcessId(hWindow, &pid);
+
+    hProcess = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, pid);
+    wallhackAddr = config.offset;
+
+    setStateFromMemory();
+}
+
+bool Wallhack::isAvailable() {
+    if (hWindow != nullptr) {
+        return true;
+    }
+
+    return false;
+}
+
+bool Wallhack::isActive() const {
+    return state == ENABLE_WALLHACK;
+}
+
+bool Wallhack::isFlashing() const {
+    return flashing;
+}
+
+void Wallhack::toggle() {
+    changeDrawMode(state == ENABLE_WALLHACK ? DISABLE_WALLHACK : ENABLE_WALLHACK, &state);
+}
+
+void Wallhack::toggleFlashing() {
+    if (state == ENABLE_WALLHACK) {
+        toggle();
+    }
+
+    flashing = !flashing;
+}
--- wallhack.h
+++ wallhack.h
@@ -17,16 +17,13 @@
 
 using namespace std;
 
-class MemAccessException : public std::exception {
+class MemAccessException : public exception {
 private:
     string message;
 
 public:
-    explicit MemAccessException(const string& msg) : message(msg) {}
-
-    const char* what() const noexcept override {
-        return message.c_str();
-    }
+    explicit MemAccessException(const string& msg);
+    const char* what() const noexcept override;
 };
 
 class Wallhack {
@@ -40,74 +37,18 @@ private:
 
     uintptr_t wallhackAddr;
 
-    void setStateFromMemory() {
-        ReadProcessMemory(hProcess, (void*) wallhackAddr, &state, sizeof(state), nullptr);
-    }
-
-    void changeDrawMode(short mode, short* statePtr) {
-        MEMORY_BASIC_INFORMATION mbi;
-
-        if (VirtualQueryEx(hProcess, (LPVOID) wallhackAddr, &mbi, sizeof(mbi)) == 0) {
-            throw MemAccessException("Error: invalid address " + to_string(wallhackAddr) + ". Code: " + to_string(GetLastError()));
-        }
-
-        if (mbi.State != MEM_COMMIT) {
-            throw MemAccessException("Error: memory is not committed");
-        }
-
-        DWORD oldProtect;
-
-        if (VirtualProtectEx(hProcess, (LPVOID) wallhackAddr, sizeof(mode), PAGE_READWRITE, &oldProtect)) {
-            if (!WriteProcessMemory(hProcess, (LPVOID) wallhackAddr, &mode, sizeof(mode), nullptr)) {
-                throw MemAccessException("Error: cannot write value in memory. Code: " + to_string(GetLastError()));
-            }
-
-            *statePtr = mode;
-        } else {
-            throw MemAccessException("Error: Cannot unprotect memory. Code: " + to_string(GetLastError()));
-        }
-
-        VirtualProtectEx(hProcess, (LPVOID) wallhackAddr, sizeof(mode), oldProtect, &oldProtect);
-    }
+    void setStateFromMemory();
+    void changeDrawMode(short mode, short* statePtr);
 
 public:
-    explicit Wallhack(const Config &config) {
-        hWindow = FindWindowA(nullptr, config.window.c_str());
-        GetWindowThreadProcessId(hWindow, &pid);
+    explicit Wallhack(const Config& config);
 
-        hProcess = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, pid);
-        wallhackAddr = config.offset;
+    bool isAvailable();
+    bool isActive();
+    bool isFlashing();
 
-        setStateFromMemory();
-    }
-
-    bool isAvailable() {
-        if (hWindow != nullptr) {
-            return true;
-        }
-
-        return false;
-    }
-
-    bool isActive() const {
-        return state == ENABLE_WALLHACK;
-    }
-
-    bool isFlashing() const {
-        return flashing;
-    }
-
-    void toggle() {
-        changeDrawMode(state == ENABLE_WALLHACK ? DISABLE_WALLHACK : ENABLE_WALLHACK, &state);
-    }
-
-    void toggleFlashing() {
-        if (state == ENABLE_WALLHACK) {
-            toggle();
-        }
-
-        flashing = !flashing;
-    }
+    void toggle();
+    void toggleFlashing();
 };
 
 #endif
